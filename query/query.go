cf6fde6582578b0f4dd25094349a8ccd3ee1bafe
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-03-13 14:35:00 -0400

    Add parameterized query

---
+++ query/query.go
@@ -0,0 +1,74 @@
+package query
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"cloud.google.com/go/bigquery"
+	"github.com/m-lab/go/logx"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"google.golang.org/api/iterator"
+)
+
+var (
+	// RowDelay pauses query result collection on every row.
+	RowDelay = time.Duration(0)
+)
+
+var (
+	repackerQueryRows = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Name: "repacker_query_results_total",
+			Help: "The number of query rows processed",
+		},
+	)
+	repackerQueryErrors = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Name: "repacker_query_read_errors_total",
+			Help: "The number of query errors",
+		},
+	)
+)
+
+// Run collects all rows of the parameterized type from a job based on the given
+// query and any query parameters.
+func Run[Row any](ctx context.Context, c *bigquery.Client, query string, params map[string]interface{}) ([]Row, error) {
+	t := time.Now()
+
+	var queryParams []bigquery.QueryParameter
+	for key, value := range params {
+		queryParams = append(queryParams, bigquery.QueryParameter{Name: key, Value: value})
+	}
+
+	q := c.Query(query)
+	q.QueryConfig = bigquery.QueryConfig{
+		Q:          query,
+		Priority:   bigquery.BatchPriority,
+		Parameters: queryParams,
+	}
+	it, err := q.Read(ctx)
+	if err != nil {
+		repackerQueryErrors.Inc()
+		return nil, err
+	}
+	results := make([]Row, 0, 1000)
+	logx.Debug.Printf("context %p: start query rows: %d, %s", ctx, it.TotalRows, time.Since(t))
+	for {
+		var row Row
+		err = it.Next(&row)
+		if err != nil {
+			break
+		}
+		results = append(results, row)
+		time.Sleep(RowDelay)
+		repackerQueryRows.Inc()
+	}
+	if err != iterator.Done {
+		repackerQueryErrors.Inc()
+		return nil, err
+	}
+	log.Printf("context %p: complete query rows: %d, %s", ctx, it.TotalRows, time.Since(t))
+	return results, nil
+}
---
+++ query/query_test.go
@@ -0,0 +1,72 @@
+package query
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+
+	"cloud.google.com/go/bigquery"
+	"google.golang.org/api/option"
+
+	"github.com/goccy/bigquery-emulator/server"
+	"github.com/m-lab/go/testingx"
+)
+
+func TestRun(t *testing.T) {
+	type Row struct {
+		Date string
+	}
+	tests := []struct {
+		name    string
+		date    string
+		query   string
+		want    []Row
+		wantErr bool
+	}{
+		{
+			name:  "success-template-variable",
+			date:  "2023-01-01",
+			query: `SELECT CAST(date AS STRING) as Date FROM (SELECT DATE("2023-01-01") as date) WHERE date = @date`,
+			want: []Row{
+				{Date: "2023-01-01"},
+			},
+		},
+		{
+			name:    "error-canceled-context",
+			wantErr: true,
+		},
+	}
+
+	bqServer, err := server.New(server.TempStorage)
+	testingx.Must(t, err, "failed to start fake bq server")
+	defer bqServer.Close()
+	bqServer.SetProject("test-project")
+	testServer := bqServer.TestServer()
+	defer testServer.Close()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			client, err := bigquery.NewClient(context.Background(), "test-project", option.WithEndpoint(testServer.URL), option.WithoutAuthentication())
+			testingx.Must(t, err, "failed to create bq client")
+			defer client.Close()
+
+			ctx, cancel := context.WithCancel(context.Background())
+			if tt.wantErr {
+				cancel()
+			}
+			defer cancel()
+
+			p := map[string]interface{}{
+				"date": tt.date,
+			}
+			result, err := Run[Row](ctx, client, tt.query, p)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Run() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !cmp.Equal(result, tt.want) {
+				t.Errorf("Run() = %v, want %v", result, tt.want)
+			}
+		})
+	}
+}
