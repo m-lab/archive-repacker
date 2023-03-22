eb629b46da73d24cf9272e8a05f3b6fa31dfc966
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-03-21 22:21:23 -0400

    Rename references to archive.Target

--- archive/target.go
+++ archive/target.go
@@ -24,59 +24,59 @@ var (
 	)
 )
 
-// A Writer represents a single, compressed, tar archive containing files to be
+// A Target represents a single, compressed, tar archive containing files to be
 // uploaded to GCS.
-type Writer struct {
+type Target struct {
 	// Count is the number of files written to the archive.
 	Count      int
 	bytes      *bytes.Buffer
-	tarWriter  *tar.Writer
-	gzipWriter *gzip.Writer
+	tarTarget  *tar.Writer
+	gzipTarget *gzip.Writer
 }
 
-// NewWriter creates a new Writer for adding files to a compressed tar archive.
-func NewWriter() *Writer {
+// NewTarget creates a new Target for adding files to a compressed tar archive.
+func NewTarget() *Target {
 	buffer := &bytes.Buffer{}
-	gzipWriter := gzip.NewWriter(buffer)
-	tarWriter := tar.NewWriter(gzipWriter)
-	return &Writer{
+	gzipTarget := gzip.NewWriter(buffer)
+	tarTarget := tar.NewWriter(gzipTarget)
+	return &Target{
 		bytes:      buffer,
-		tarWriter:  tarWriter,
-		gzipWriter: gzipWriter,
+		tarTarget:  tarTarget,
+		gzipTarget: gzipTarget,
 	}
 }
 
-// AddFile appends a single file to the Writer with the given header and file contents.
-func (ar *Writer) AddFile(h *tar.Header, contents []byte) error {
+// AddFile appends a single file to the Target with the given header and file contents.
+func (ar *Target) AddFile(h *tar.Header, contents []byte) error {
 	if h == nil {
 		return nil
 	}
-	err := ar.tarWriter.WriteHeader(h)
+	err := ar.tarTarget.WriteHeader(h)
 	if err != nil {
 		return fmt.Errorf("writing header for %q failed: %w", h.Name, err)
 	}
 	for total := 0; total < len(contents); {
-		n, err := ar.tarWriter.Write(contents[total:])
+		n, err := ar.tarTarget.Write(contents[total:])
 		if err != nil {
 			return fmt.Errorf("writing contents for %q failed: %w", h.Name, err)
 		}
 		total += n
 	}
 	// Flush the data so that our in-memory filesize is accurate.
-	rtx.Must(ar.tarWriter.Flush(), "Could not flush the tarWriter")
-	rtx.Must(ar.gzipWriter.Flush(), "Could not flush the gzipWriter")
+	rtx.Must(ar.tarTarget.Flush(), "Could not flush the tarTarget")
+	rtx.Must(ar.gzipTarget.Flush(), "Could not flush the gzipTarget")
 	ar.Count++
 	return nil
 }
 
-// Close closes the Writer archive.
-func (ar *Writer) Close() error {
-	ar.tarWriter.Close()
-	return ar.gzipWriter.Close()
+// Close closes the Target archive.
+func (ar *Target) Close() error {
+	ar.tarTarget.Close()
+	return ar.gzipTarget.Close()
 }
 
-// Upload writes the completed Writer archive contents to the named GCS Path.
-func (ar *Writer) Upload(ctx context.Context, client *storage.Client, p *Path) error {
+// Upload writes the completed Target archive contents to the named GCS Path.
+func (ar *Target) Upload(ctx context.Context, client *storage.Client, p *Path) error {
 	sctx, cancel := context.WithTimeout(ctx, 20*time.Minute)
 	defer cancel()
 
