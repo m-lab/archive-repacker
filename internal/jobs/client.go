3b3e5e9eeb797882352b4537fbf833b293a88a61
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-03-22 14:40:19 -0400

    Dedup request logic

--- internal/jobs/client.go
+++ internal/jobs/client.go
@@ -73,24 +73,19 @@ func (c *Client) Lease(ctx context.Context) (string, error) {
 func (c *Client) Update(ctx context.Context, date string) error {
 	l := *c.Server
 	l.Path = "/v1/update"
-	q := l.Query()
-	q.Add("date", date)
-	l.RawQuery = q.Encode()
-	resp, err := makeRequest(ctx, c.Client, &l)
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode != http.StatusOK {
-		return errors.New("bad status: " + resp.Status)
-	}
-	defer resp.Body.Close()
-	return nil
+	return c.request(ctx, date, l)
 }
 
 // Complete accepts a previously leased date and marks it as complete.
 func (c *Client) Complete(ctx context.Context, date string) error {
 	l := *c.Server
 	l.Path = "/v1/complete"
+	return c.request(ctx, date, l)
+}
+
+// request issues a connection to given url with a single date parameter. Any
+// non-200 response is an error.
+func (c *Client) request(ctx context.Context, date string, l url.URL) error {
 	q := l.Query()
 	q.Add("date", date)
 	l.RawQuery = q.Encode()
@@ -99,7 +94,7 @@ func (c *Client) Complete(ctx context.Context, date string) error {
 		return err
 	}
 	if resp.StatusCode != http.StatusOK {
-		return errors.New("bad status: " + resp.Status)
+		return errors.New("wrong response status: " + resp.Status)
 	}
 	defer resp.Body.Close()
 	return nil
