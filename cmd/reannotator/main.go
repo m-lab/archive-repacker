a6cb10f57d7de9f85dc13bed3407975236fdf14d
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-03-24 14:19:04 -0400

    Add comments and flag descriptions

--- cmd/reannotator/main.go
+++ cmd/reannotator/main.go
@@ -26,15 +26,14 @@ import (
 )
 
 var (
-	queryset    = flagx.File{}
+	querystr    = flagx.File{}
 	routeviewv4 = flagx.URL{}
 	routeviewv6 = flagx.URL{}
 	asnameurl   = flagx.URL{}
 	jobservice  = flagx.URL{}
 	bqDelay     = time.Millisecond
-
-	project   string
-	outBucket string
+	project     string
+	outBucket   string
 
 	mainCtx, mainCancel = context.WithCancel(context.Background())
 
@@ -66,29 +65,28 @@ var (
 )
 
 func init() {
-	flag.Var(&queryset, "query", "filename with query to generate repacker archive list and options")
-	flag.Var(&routeviewv4, "routeview-v4.url", "The URL for the RouteViewIPv4 file containing ASN metadata. gs:// and file:// schemes accepted.")
-	flag.Var(&routeviewv6, "routeview-v6.url", "The URL for the RouteViewIPv6 file containing ASN metadata. gs:// and file:// schemes accepted.")
-	flag.Var(&asnameurl, "asname.url", "The URL for the ASName CSV file containing a mapping of AS numbers to AS names provided by IPInfo.io")
-	flag.Var(&jobservice, "jobservice.url", "The URL for the job service providing dates to process")
+	flag.Var(&querystr, "query", "Filename with date parameterized query to generate annotation archive list.")
+	flag.Var(&routeviewv4, "routeview-v4.url", "The URL for the RouteViewIPv4 file containing prefix2as data. gs:// and file:// schemes accepted.")
+	flag.Var(&routeviewv6, "routeview-v6.url", "The URL for the RouteViewIPv6 file containing prefix2as data. gs:// and file:// schemes accepted.")
+	flag.Var(&asnameurl, "asname.url", "The URL for the ASName CSV file containing a mapping of AS numbers to AS names provided by IPInfo.io.")
+	flag.Var(&jobservice, "jobservice.url", "The URL for the job service providing dates to process.")
 	flag.StringVar(&project, "project", "", "GCP project name.")
-	flag.StringVar(&outBucket, "output", "soltesz-tfstate-0", "")
-	flag.DurationVar(&bqDelay, "bq-delay", time.Millisecond, "how long to wait between bq row requests")
+	flag.StringVar(&outBucket, "output", "annotation2-output-mlab-sandbox", "Write generated archives to this GCS Bucket.")
+	flag.DurationVar(&bqDelay, "bq-delay", time.Millisecond, "Pause collection of each BigQuery row.")
 
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 }
 
 func main() {
+	defer mainCancel()
 	flag.Parse()
 	rtx.Must(flagx.ArgsFromEnv(flag.CommandLine), "Could not get args from env")
-	prometheusx.MustServeMetrics()
-
-	defer mainCancel()
 	if project == "" {
 		log.Fatal("-project is a required flag")
 	}
-	// TODO: add http client with timeouts.
-	c := jobs.NewClient(jobservice.URL, http.DefaultClient)
+
+	prometheusx.MustServeMetrics()
+	jc := jobs.NewClient(jobservice.URL, http.DefaultClient)
 	sclient, err := storage.NewClient(mainCtx, option.WithScopes(storage.ScopeReadWrite))
 	rtx.Must(err, "failed to create new read/write storage client")
 	p := annotation.NewProcessor(
@@ -104,49 +102,52 @@ func main() {
 	//
 	// The aggregate quota is ~500Mbps.
 	// * For 64 clients, that's < 8Mbps each.
-	// * If each row is ~1k a 1ms pause is about 1Mbps
-	// * 500us will add ~0.5s per 1000 results.
-	// * ~128Mbps aggregate for 64 clients.
-	query.RowDelay = time.Millisecond / 2
+	// * If each row is ~1k a 1ms pause is about 1Mbps, etc.
+	// * .5ms would be ~128Mbps aggregate for 64 clients.
+	query.RowDelay = bqDelay
 
-	// Run query to get list of archives to operate on.
+	// Enable 'StorageReadClient' so bigquery results are read *much* more quickly.
 	bqclient, err := bigquery.NewClient(mainCtx, project)
 	rtx.Must(err, "failed to create bigquery client")
 	err = bqclient.EnableStorageReadClient(mainCtx)
 	rtx.Must(err, "failed to enable storage read client")
 
+	// Create a process manager for the annotation result type. The result
+	// type must correspond to the rows returned by the configured query.
 	r := process.Manager[annotation.Result]{
-		Jobs:        c,
+		Jobs:        jc,
 		Process:     p,
-		OutBucket:   outBucket,
 		QueryClient: bqiface.AdaptClient(bqclient),
-		Query:       queryset.Content(),
+		Query:       querystr.Content(),
 	}
-	logx.Debug.Println(queryset.Content())
+	logx.Debug.Println(querystr.Content())
+
+	// Process jobs indefinitely.
 	for {
-		date, err := c.Lease(mainCtx)
+		date, err := jc.Lease(mainCtx)
 		switch err {
 		case jobs.ErrEmpty:
 			log.Println("Work queue empty; exiting")
 			return
 		case jobs.ErrWait:
-			log.Println("Waiting for job; sleeping 1m")
+			log.Println("Work queue pending; waiting for 1m")
 			time.Sleep(time.Minute)
 			continue
 		}
 		rtx.Must(err, "failed to request job lease")
-
-		t := time.Now()
-		log.Println("Processing:", date)
 		d, err := time.Parse(timex.YYYYMMDDWithDash, date)
 		rtx.Must(err, "failed to parse job date")
+
+		t := time.Now()
+		log.Println("Starting date:", date)
 		repackerDate.WithLabelValues("starting").Set(float64(d.Unix()))
 		repackerDatesStarted.Inc()
 
-		r.ProcessDate(mainCtx, date)
+		err = r.ProcessDate(mainCtx, date)
+		rtx.Must(err, "failed to run job to completion: %s", date)
 
-		err = c.Complete(mainCtx, date)
-		rtx.Must(err, "failed to run complete with job service")
+		err = jc.Complete(mainCtx, date)
+		rtx.Must(err, "failed to complete job with job service: %s", date)
 		log.Println("Completed date:", date, time.Since(t))
 		repackerDate.WithLabelValues("completed").Set(float64(d.Unix()))
 		repackerDateCompletionTime.Observe(time.Since(t).Seconds())
