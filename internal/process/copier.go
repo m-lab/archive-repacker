59e3248296e82b6ce70924e76f7f1d53a9147916
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-03-28 16:22:55 -0400

    Add process.Copier with tests

---
+++ internal/process/copier.go
@@ -0,0 +1,41 @@
+package process
+
+import (
+	"context"
+	"log"
+
+	"github.com/m-lab/archive-repacker/internal/jobs"
+)
+
+// Renamer is an interface for types that support renaming.
+type Renamer interface {
+	List(ctx context.Context, date string) ([]string, error)
+	Rename(ctx context.Context, url string) (string, error)
+}
+
+// Copier manages bulk rename operations.
+type Copier struct {
+	Jobs    *jobs.Client
+	Process Renamer
+}
+
+// ProcessDate applies the renamer to the given date.
+func (c *Copier) ProcessDate(ctx context.Context, date string) error {
+	l, err := c.Process.List(ctx, date)
+	if err != nil {
+		return err
+	}
+	for i := range l {
+		if i%1000 == 0 && c.Jobs != nil {
+			// Update every 100 operations.
+			log.Printf("Renamed %d objects for %s", i, date)
+			c.Jobs.Update(ctx, date)
+		}
+		_, err := c.Process.Rename(ctx, l[i])
+		if err != nil {
+			log.Printf("failed to rename %q: %v", l[i], err)
+			return err
+		}
+	}
+	return nil
+}
