74df8834522447092c24c3897dd5947fc0f0b0c1
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-04-17 15:19:50 -0400

    Simplify hopannotation query

--- k8s/reannotator-query-hopannotation1.sql
+++ k8s/reannotator-query-hopannotation1.sql
@@ -1,13 +1,9 @@
-WITH parse_ips AS (
-  SELECT parser, SPLIT(id, '_')[SAFE_OFFSET(2)] as DstIP
-  FROM `measurement-lab.ndt_raw.hopannotation1`
-  WHERE date = @date
-)
-
 SELECT
   parser.ArchiveURL as ArchiveURL,
-  ARRAY_AGG(STRUCT(parser.Filename, DstIP)) as Files
+  STRUCT("" as Filename, "" as DstIP) as Files
 FROM
-  parse_ips
+  `measurement-lab.ndt_raw.hopannotation1`
+WHERE
+  date = @date
 GROUP BY
   ArchiveURL
